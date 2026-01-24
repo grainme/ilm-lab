594d04e0d0cfda9456458123605c018262a9bc15
grainme <marouanebouf3@gmail.com>
2026-01-24 16:41:35 +0100

    Add README.md for lab1-chi

---
+++ CS/Web/REST_API/golang/lab2-chi/cmd/api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/google/uuid"
+	"github.com/grainme/movie-api/internal/domain"
+	handlers "github.com/grainme/movie-api/internal/handler"
+	"github.com/grainme/movie-api/internal/repository/memory"
+	"github.com/grainme/movie-api/internal/service"
+)
+
+func main() {
+	movies := []*domain.Movie{
+		{Id: uuid.New(), Title: "BadMovie1", Rating: 2},
+		{Id: uuid.New(), Title: "BadMovie2", Rating: 5},
+	}
+
+	movieRepo := memory.NewMemoryMovieRepository(movies)
+	movieService := service.NewMovieService(movieRepo)
+	movieHandler := handlers.NewMovieHandler(movieService)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/movies", movieHandler.GetAllMovies)
+	r.Get("/movies/{id}", movieHandler.GetMovieById)
+	r.Post("/movies", movieHandler.AddMovie)
+	r.Put("/movies/{id}", movieHandler.UpdateMovieById)
+	r.Delete("/movies/{id}", movieHandler.DeleteById)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
---
+++ CS/Web/REST_API/golang/lab2-chi/internal/service/movie_service.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/grainme/movie-api/internal/domain"
+	"github.com/grainme/movie-api/internal/repository"
+)
+
+type MovieService struct {
+	movieRepo repository.MovieRepository
+}
+
+func NewMovieService(repo repository.MovieRepository) *MovieService {
+	return &MovieService{
+		movieRepo: repo,
+	}
+}
+
+func (s *MovieService) GetAllMovies(ctx context.Context) []*domain.Movie {
+	// should we have any other logic (validation...)?
+	movies := s.movieRepo.GetAllMovies(ctx)
+	return movies
+}
+
+func (s *MovieService) GetMovieById(ctx context.Context, id uuid.UUID) (*domain.Movie, error) {
+	movie, err := s.movieRepo.GetMovieById(ctx, id)
+	return movie, err
+}
+
+func (s *MovieService) AddMovie(ctx context.Context, movie *domain.Movie) (*domain.Movie, error) {
+	if movie == nil {
+		return nil, domain.ErrInvalidMovie
+	}
+	if movie.Rating < 0 || movie.Rating > 10 {
+		return nil, domain.ErrInvalidRating
+	}
+	if movie.Title == "" || len(movie.Title) > 40 {
+		return nil, domain.ErrInvalidMovie
+	}
+
+	movie, err := s.movieRepo.AddMovie(ctx, movie)
+	return movie, err
+}
+
+func (s *MovieService) UpdateMovieById(ctx context.Context, id uuid.UUID, newRating int) (*domain.Movie, error) {
+	if newRating < 0 || newRating > 10 {
+		return nil, domain.ErrInvalidRating
+	}
+	movie, err := s.movieRepo.UpdateMovieById(ctx, id, newRating)
+	return movie, err
+}
+
+func (s *MovieService) DeleteMovieById(ctx context.Context, id uuid.UUID) error {
+	err := s.movieRepo.DeleteMovieById(ctx, id)
+	return err
+}
